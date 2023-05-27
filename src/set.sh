1577f7f1919c25376358a0b02ec01cb60deb4134
AnsonCar <anson1tsang@gmail.com>
2023-05-28 05:26:26 +0800

    omg我就泥死啦...

--- src/set.sh
+++ src/set.sh
@@ -1,4 +1,3 @@
-touch pyproject.toml
 python3 -m venv .venv
 source $(pwd)/.venv/bin/activate
 pip install --upgrade pip
@@ -9,20 +8,16 @@ pip install matplotlib
 pip install pyecharts
 pip install notebook
 pip install jupyterlab
+pip install flit
 pip freeze > requirements.txt
 
-# git clone https://github.com/pyecharts/pyecharts-gallery.git
-# python3 -m pip install --user --upgrade setuptools wheel twine
-# python3 -m pip install setuptools wheel twine
-# python3 setup.py sdist bdist_wheel
+# flit build
+# python3 -m twine upload --repository testpypi dist/*
 
 # python3 -m pip install --upgrade build
 # python3 -m pip install --upgrade twine
-# python3 -m build
-# python3 -m twine upload --repository https://pypi.org/pyday/ dist/*
-# python3 -m twine upload dist/*--repository
 
+# pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple pyday-AnsonCar==0.0.17
 
-# open ~/.pypirc
-
-# python3 -m pip install --index-url https://pypi.org/pyday/ --no-deps pyday_AnsonCar
+# touch pyproject.toml
+# git clone https://github.com/pyecharts/pyecharts-gallery.git
